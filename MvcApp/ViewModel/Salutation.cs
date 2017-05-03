3e7df87c8cc1b3ea584931b88ff97213fb509c1f
Albert Weinert <info@der-albert.com>
2017-05-03 22:13:16 +0200

    TagHelper

--- MvcApp/ViewModel/Salutation.cs
+++ MvcApp/ViewModel/Salutation.cs
@@ -4,8 +4,11 @@ namespace MvcApp.ViewModel
 {
     public enum Salutation
     {
+        [Display(Name = "Unbekannt")]
         Unknown,
+        [Display(Name = "Herr")]
         Mr,
+        [Display(Name = "Frau")]
         Ms
     }
 }
