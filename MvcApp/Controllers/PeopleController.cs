6f5273a3530903bd125a4ecb259dbb1769434c5d
Albert Weinert <info@der-albert.com>
2017-05-04 06:36:46 +0200

    Edit Person

--- MvcApp/Controllers/PeopleController.cs
+++ MvcApp/Controllers/PeopleController.cs
@@ -17,9 +17,39 @@ namespace MvcApp.Controllers
         {
 
             var people = from p in _dbContext.People
-                select new PersonModel(p.Salutation, p.FirstName, p.LastName);
+                select new PersonModel(p.Salutation, p.FirstName, p.LastName) {Id = p.Id};
 
             return View(people);
+        }
+
+        public IActionResult Edit(long id)
+        {
+            var person = _dbContext.People.Find(id);
+
+            if (person == null)
+            {
+                return NotFound();
+            }
+
+            return View(new PersonModel(person.Salutation, person.FirstName, person.LastName) { Id = person.Id });
+        }
+
+        [HttpPost]
+        public IActionResult Edit(long id, PersonModel model)
+        {
+            var person = _dbContext.People.Find(id);
+
+            if (person == null)
+            {
+                return NotFound();
+            }
+
+            person.Salutation = model.Salutation;
+            person.FirstName = model.FirstName;
+            person.LastName = model.LastName;
+
+            _dbContext.SaveChanges();
+            return RedirectToAction("Index");
         }
     }
 }
--- MvcApp/ViewModel/PersonModel.cs
+++ MvcApp/ViewModel/PersonModel.cs
@@ -4,12 +4,18 @@ namespace MvcApp.ViewModel
 {
     public class PersonModel
     {
+        public PersonModel()
+        {
+            
+        }
         public PersonModel(Salutation salutation, string firstName, string lastName)
         {
             Salutation = salutation;
             FirstName = firstName;
             LastName = lastName;
         }
+
+        public long Id { get; set; }
 
         public string FirstName { get; set; }
         public string LastName { get; set; }
